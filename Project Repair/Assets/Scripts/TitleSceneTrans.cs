17d9d1728ce3157c1830091a0b9103130642721d
Atsu30 <atsmr.30autb@gmail.com>
2020-02-02 14:55:31 +0900

    タイトルシーンの遷移を修正

--- Project Repair/Assets/Scripts/TitleSceneTrans.cs
+++ Project Repair/Assets/Scripts/TitleSceneTrans.cs
@@ -18,6 +18,6 @@ public class TitleSceneTrans : MonoBehaviour
     }
     public void Loadmaingame()
     {
-        SceneManager.LoadScene("GameMasterScene");
+        SceneManager.LoadScene("GameMasterScene_mori");
     }
 }
