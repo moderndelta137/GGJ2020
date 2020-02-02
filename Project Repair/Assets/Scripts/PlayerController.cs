54f68afc21979391df5754c7f8fae3e4b61ef824
sattch02 <sattch02@gmail.com>
2020-02-02 15:13:32 +0900

    speedUpのbugfix

--- Project Repair/Assets/Scripts/PlayerController.cs
+++ Project Repair/Assets/Scripts/PlayerController.cs
@@ -57,6 +57,7 @@ public class PlayerController : MonoBehaviour
     private Animator player1Animator;
     private Animator player2Animator;
 
+    public bool playerSpdFlg = false;
     public bool playerStunFlg = false;
     public bool playerToughFlg = false;
 
@@ -253,6 +254,9 @@ public class PlayerController : MonoBehaviour
                     // アイテム触れたらアイテムObj消す
                     Destroy(other.gameObject);
                     //
+
+                    if (playerSpdFlg) break;
+                    playerSpdFlg = true;
                     Move_speed = Move_speed * 2;
                     speedUpEffect.SetActive(true);
                     Debug.Log("speedup");
@@ -333,6 +337,7 @@ public class PlayerController : MonoBehaviour
 
     public void returnMoveSpeed()
     {
+        playerSpdFlg = false;
         Move_speed = Move_speed / 2;
         speedUpEffect.SetActive(false);
     }
